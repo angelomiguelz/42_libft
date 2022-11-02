dc81f206f6c280abfd6255d83647185b6a0bc6bd
BuildTools <unconfigured@null.spigotmc.org>
2022-11-02 10:11:59 +0000

    upload

---
+++ ft_putstr_fd.c
@@ -0,0 +1,20 @@
+/*
+*	DESCRIPTION
+*	Outputs the string ’s’ to the given file descriptor.
+*	PARAMETERS
+*	#1. The string to output.
+*	#2. The file descriptor on which to write.
+*	RETURN VALUES
+*	-
+*/
+
+#include "libft.h"
+
+void	ft_putstr_fd(char *s, int fd)
+{
+	size_t	i;
+
+	i = -1;
+	while (s[++i])
+		ft_putchar_fd(s[i], fd);
+}
